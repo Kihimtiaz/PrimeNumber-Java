a1695be0a593986efe90c670cdc39a438884a418
Imtiaz <kihimtiaz1030@gmail.com>
2026-09-02 23:51:08 +0600

    First Commit

---
+++ PrimeNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+         int num, count=0;
+         System.out.print("Enter an positive Integer: ");
+         num= input.nextInt();
+
+        for(int i=2; i<num; i++){
+            if(num%i==0){
+                count=count++;
+                break;
+            }
+        }
+        if(count==0){
+            System.out.println(num+ " is a prime number");
+        }
+        else{
+            System.out.println(num+ " is not a prime number");
+        }
+    }
+}
